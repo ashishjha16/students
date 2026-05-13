878e8b5c1b7d72bbb86c4ae0744bc5cc205d6369
ashishjha16 <ashishjha7628@gmail.com>
2026-05-13 08:49:50 +0530

    ready for django deployment

--- student_management_system/student_management_system/settings.py
+++ student_management_system/student_management_system/settings.py
@@ -82,18 +82,11 @@ WSGI_APPLICATION = "student_management_system.wsgi.application"
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        #"ENGINE": "django.db.backends.sqlite3",
-        #"NAME": BASE_DIR / "db.sqlite3",
-         'ENGINE':'django.db.backends.mysql',
-         'NAME':'student_management_system',
-         'USER':'student_management_system',
-         'PASSWORD':'student_management_password',
-         'HOST':'localhost',
-         'PORT':'3306'
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
