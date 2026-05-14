37ea986e5c0aa8cdceb91d3a5c1962165fae9f9e
ashishjha16 <ashishjha7628@gmail.com>
2026-05-14 08:34:44 +0530

    fix static files for render

--- student_management_system/student_management_system/settings.py
+++ student_management_system/student_management_system/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -124,6 +125,11 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'student_management_app' / 'static',]
 AUTH_USER_MODEL="student_management_app.CustomUser"
 AUTHENTICATION_BACKENDS=['student_management_app.EmailBackEnd.EmailBackEnd']
 
